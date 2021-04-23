73b2eeda09a8b7c35a95f96ee9be0f550e675205
MET <mustafatpe23@gmail.com>
2021-04-24 00:23:08 +0300

    btk last update

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart-service.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  productAddedToCart=false
+  products:Product[]=[]
+  constructor(private productService:ProductService,private cartService:CartService) { }
 
   ngOnInit(): void {
+    this.getAll();
   }
-
+  getAll(){
+    this.productService.getProducts().subscribe(response=>this.products=response.data)
+  }
+  addToCart(product:Product){
+    this.productAddedToCart=false
+    this.cartService.add({productId:product.id,userId:6,count:1,id:0}).subscribe(response=>{
+      this.productAddedToCart=true
+    })
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  constructor() { }
+  apiUrl="https://localhost:44314/api/products/"
+  constructor(private httpClient:HttpClient) { }
+  getProducts():Observable<ListResponseModel<Product>>{
+    let api=this.apiUrl+"getall"
+    return this.httpClient.get<ListResponseModel<Product>>(api)
+  }
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+    let api=this.apiUrl+"getbycategory?categoryId="
+    return this.httpClient.get<ListResponseModel<Product>>(api+categoryId)
+  }
+  add(product:Product):Observable<ResponseModel>{
+    let api=this.apiUrl+"add"
+    return this.httpClient.post<ResponseModel>(api,product)
+  }
 }
